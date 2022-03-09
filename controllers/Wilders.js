2a3b776e37ef506c1ab5e194f1dcb33d21bf3089
Margaux De Filippis <margaux.defilippis@gmail.com>
2022-03-09 18:48:01 +0100

    errors handled by higher order function

--- controllers/Wilders.js
+++ controllers/Wilders.js
@@ -1,68 +1,45 @@
 const WilderModel = require("../models/Wilder");
 
 module.exports = {
-    create: async (req, res) => {
+    create: async (req, res, next) => {
         try {
-            await WilderModel.init()
+            WilderModel.init()
             const newWilder = new WilderModel(req.body);
-
             const result = await newWilder.save();
             res.json({ success: true, result: result });
         } catch (err) {
             if (err.code === 11000) {
                 res.status(400).json({ message: 'Name already taken' });
-            } else {
-                console.error(err);
-                res.status(500).json({ message: 'db_error' });
             }
+            throw error;
         }
     },
 
-    findAll: async (req, res) => {
-        try {
-            const wilders = await WilderModel.find();
-            res.json(wilders);
-        } catch (err) {
-            console.error(err);
-            res.status(500).json({ message: 'server_error' });
-        }
+    findAll: async (req, res, next) => {
+        const wilders = await WilderModel.find();
+        res.json(wilders);
     },
 
-    findOne: async (req, res) => {
-        try {
-            const wilder = await WilderModel.findById(req.params.id);
+    findOne: async (req, res, next) => {
+        const wilder = await WilderModel.findById(req.params.id);
+        res.json(wilder);
+    },
+
+    update: async (req, res, next) => {
+        const wilder = await WilderModel.findById(req.params.id);
+        if (wilder) {
+            Object.assign(wilder, req.body);
+
+            await wilder.save();
             res.json(wilder);
-        } catch (err) {
-            console.error(err);
-            res.status(500).json({ message: 'server_error' });
+        } else {
+            res.status(404).json({ message: 'not_found' });
         }
     },
 
-    update: async (req, res) => {
-        try {
-            const wilder = await WilderModel.findById(req.params.id);
-            if (wilder) {
-                Object.assign(wilder, req.body);
-
-                await wilder.save();
-                res.json(wilder);
-            } else {
-                res.status(404).json({ message: 'not_found' });
-            }
-        } catch (err) {
-            console.error(err);
-            res.status(500).json({ message: 'server_error' });
-        }
-    },
-
-    delete: async (req, res) => {
-        try {
-            const wilder = await WilderModel.findById(req.params.id)
-            await wilder.remove();
-            res.json(wilder);
-        } catch (err) {
-            console.error(err);
-            res.status(500).json({ message: 'server_error' });
-        }
+    delete: async (req, res, next) => {
+        const wilder = await WilderModel.findById(req.params.id)
+        await wilder.remove();
+        res.json(wilder);
     },
 };
--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const mongoose= require("mongoose");
 const wilderController = require("./controllers/Wilders");
 const app = express();
 
+function execAsyncHandler(handler) {
+    return async function (req, res, next) {
+        try {
+            await handler(req, res, next);
+        } catch {
+            next(err);
+        }
+    }
+};
+
 //Database
 mongoose
     .connect("mongodb://127.0.0.1:27017/wilderdb", {
@@ -19,11 +29,17 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.send("Hello World");
 }); 
-app.post("/api/wilders", wilderController.create);
-app.get("/api/wilders", wilderController.findAll);
-app.get("/api/wilders/:id", wilderController.findOne);
-app.put("/api/wilders/:id", wilderController.update);
-app.delete("/api/wilders/:id", wilderController.delete);
+app.post("/api/wilders", execAsyncHandler(wilderController.create));
+app.get("/api/wilders", execAsyncHandler(wilderController.findAll));
+app.get("/api/wilders/:id", execAsyncHandler(wilderController.findOne));
+app.put("/api/wilders/:id", execAsyncHandler(wilderController.update));
+app.delete("/api/wilders/:id", execAsyncHandler(wilderController.delete));
+
+// Handle 500 error
+app.use((error, req, res, next) => {
+    console.error(err);
+    res.status(500).json({ message: 'An error occured, sorry' });
+});
 
 // Handle 404 not found
 app.use((req, res, next) => {
